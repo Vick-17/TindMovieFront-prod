204cd474b74a0a11ca6e6190897f293b86ddb158
Vick-17 <vega.victor.0712@gmail.com>
2023-11-24 10:56:16 +0100

    DEV

--- src/service/apiService.js
+++ src/service/apiService.js
@@ -1,5 +1,5 @@
-//const API_URL = "https://backend-production-048b.up.railway.app";
-const API_URL = "http://localhost:8080";
+const API_URL = "https://backend-production-048b.up.railway.app";
+//const API_URL = "http://localhost:8080";
 
 
 
